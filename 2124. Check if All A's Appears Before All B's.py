0488d7ad70eb6070b59a602ceba07eec924ac780
Sachin Lodhi <sachinlodhi8614@gmail.com>
2022-01-28 15:51:42 +0530

    Create 2124. Check if All A's Appears Before All B's.py

---
+++ 2124. Check if All A's Appears Before All B's.py
@@ -0,0 +1,43 @@
+class Solution:
+    def checkString(self, s: str) -> bool:
+        found_b = False
+        for i in range(len(s)):
+            if s[i] == 'b': # Find first B in string
+                found_b = True
+            if s[i] == 'a' and found_b: # If 'b' has occurred and a occures after that
+                return False
+        return True
+        
+        
+'''
+Given a string s consisting of only the characters 'a' and 'b', return true if every 'a' appears before every 'b' in the string. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: s = "aaabbb"
+Output: true
+Explanation:
+The 'a's are at indices 0, 1, and 2, while the 'b's are at indices 3, 4, and 5.
+Hence, every 'a' appears before every 'b' and we return true.
+Example 2:
+
+Input: s = "abab"
+Output: false
+Explanation:
+There is an 'a' at index 2 and a 'b' at index 1.
+Hence, not every 'a' appears before every 'b' and we return false.
+Example 3:
+
+Input: s = "bbb"
+Output: true
+Explanation:
+There are no 'a's, hence, every 'a' appears before every 'b' and we return true.
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s[i] is either 'a' or 'b'.
+'''
