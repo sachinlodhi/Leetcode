5a7afda1bd51a36ed092225849d90cb7f08cb4f6
Sachin Lodhi <sachinlodhi8614@gmail.com>
2022-02-23 10:07:38 +0530

    Create 1025. Divisor Game.py

---
+++ 1025. Divisor Game.py
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        
+  
+    int ctr = 0;
+    while (n!=1){
+              ctr++;
+        n-=1;
+  
+
+    }
+    if (ctr%2==0){
+        return false;
+    }
+    return true;
+    
+    }
+};
+
+
+
+/*
+
+
+Alice and Bob take turns playing a game, with Alice starting first.
+
+Initially, there is a number n on the chalkboard. On each player's turn, that player makes a move consisting of:
+
+Choosing any x with 0 < x < n and n % x == 0.
+Replacing the number n on the chalkboard with n - x.
+Also, if a player cannot make a move, they lose the game.
+
+Return true if and only if Alice wins the game, assuming both players play optimally.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: true
+Explanation: Alice chooses 1, and Bob has no more moves.
+Example 2:
+
+Input: n = 3
+Output: false
+Explanation: Alice chooses 1, Bob chooses 1, and Alice has no more moves.
+ 
+
+Constraints:
+
+1 <= n <= 1000
+*/
