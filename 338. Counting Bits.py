70955e201a2a4f7db014f4c4e632a6b6e09d36b8
Sachin Lodhi <sachinlodhi8614@gmail.com>
2022-03-23 16:55:38 +0530

    Create 338. Counting Bits.py

---
+++ 338. Counting Bits.py
@@ -0,0 +1,45 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        return [str(bin(i))[2:].count('1') for i in range(n+1)]
+    
+    
+    '''
+    
+    ans = []
+for i in range(0, n+1):
+    ans.append(str(bin(i))[2:].count('1'))
+return ans
+
+    
+    
+    '''
+    '''
+    Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+Example 2:
+
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+ 
+
+Constraints:
+
+0 <= n <= 105
+'''
